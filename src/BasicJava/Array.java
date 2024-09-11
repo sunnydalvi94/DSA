873cf213900d401ffa6c121c043e537298896cab
sunny dalvi <55959027+sunnydalvi94@users.noreply.github.com>
2024-09-11 10:31:20 +0000

    array file modified

--- src/BasicJava/Array.java
+++ src/BasicJava/Array.java
@@ -11,10 +11,11 @@ public class Array {
         Scanner s= new Scanner(System.in);
 
       for (int i=0;i<arr.length;i++){
-        
+
         arr[i]=s.nextInt();
       }
         System.out.println("array elements are: ");
+        // this testing line for codespace
 
       for (int j : arr) {
         System.out.print(j + " ");
